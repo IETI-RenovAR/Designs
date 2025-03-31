359aac2fb80f286eb4d6dd1cf04e07e7936cf527
Ubuntu <IETI@RenovAR.m3cqph3ogoku3fcfyxdjg3yxig.bx.internal.cloudapp.net>
2025-03-31 01:25:15 +0000

    shell

--- exc.sh
+++ exc.sh
@@ -4,6 +4,6 @@ sudo docker stop designs
 git pull
 sudo docker rm designs
 sudo docker build -t designs .
-sudo docker run -d --name designs designs
+sudo docker run -d -p 8081:8081 --name designs designs
 
 
